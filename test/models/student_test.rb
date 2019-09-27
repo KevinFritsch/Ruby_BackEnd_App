1f2dd9107eeed221c045339ecc10fea0dbfd3083
KevinFritsch <kevinfritsch@hotmail.fr>
2019-09-27 13:59:47 +0200

    class student

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -5,13 +5,13 @@ class StudentTest < ActiveSupport::TestCase
   #   assert true
   # end
 
-  test "should have the necessary required validators" do
+ / test "should have the necessary required validators" do
     student = Student.new
     assert_not student.valid?
     assert_equal [:name, :last_name], student.errors.keys
     student.name = "Gerard"
     student.last_name= "Bouchard"
     assert student.valid?
-  end
-  
+  end/
+
 end
