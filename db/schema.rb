1856b50b25fab9da109b2c5d0fdb7c9f854d3037
KevinFritsch <kevinfritsch@hotmail.fr>
2019-09-27 14:44:23 +0200

    Student class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_27_114405) do
+ActiveRecord::Schema.define(version: 2019_09_27_124034) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
